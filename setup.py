68692e06d8901186f0c535f5bbb1c7ebddc039d0
manoelgadi <manoelgadi@gmail.com>
2022-09-28 14:42:07 +0200

    uploading my first python package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pythonpackagesample',
+setup(name='filltest',
       version='0.1',  # Development release
-      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
-      url='https://github.com/manoelgadi/pythonpackagesample',
+      description='Paquete test para un curso',
+      url='https://github.com/manoelgadi/filltest',
       author='Prof. Manoel Gadi',
       author_email='mfalonso@faculty.ie.edu',
       license='MIT',
-          packages=['pythonpackagesample'],
+          packages=['filltest'],
       zip_safe=False)
